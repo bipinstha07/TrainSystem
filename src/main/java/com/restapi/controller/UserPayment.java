efb9259d1106b773fe80798491c5f0d630085878
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-07-10 23:50:45 -0500

    Payment

---
+++ src/main/java/com/restapi/controller/UserPayment.java
@@ -0,0 +1,21 @@
+package com.restapi.controller;
+
+import com.restapi.dto.UserPaymentDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@AllArgsConstructor
+public class UserPayment {
+
+    private UserPaymentDto userPaymentDto;
+
+    public ResponseEntity<UserPaymentDto> payment(){
+
+        return new ResponseEntity<>(userPaymentDto, HttpStatus.CREATED);
+
+    }
+
+}
