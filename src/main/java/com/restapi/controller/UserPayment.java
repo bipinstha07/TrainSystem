2fe0c78e22acc222f13ba2b8dd17801430f0ec9a
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-07-11 23:47:46 -0500

    update payment

--- src/main/java/com/restapi/controller/UserPayment.java
+++ src/main/java/com/restapi/controller/UserPayment.java
@@ -4,6 +4,8 @@ import com.restapi.dto.UserPaymentDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/user")
@@ -12,8 +14,12 @@ public class UserPayment {
 
     private UserPaymentDto userPaymentDto;
 
-    public ResponseEntity<UserPaymentDto> payment(){
-        return new ResponseEntity<>(userPaymentDto, HttpStatus.CREATED);
+    private  PaymentService paymentService;
+
+    @PostMapping("/payment")
+    public ResponseEntity<UserPaymentDto> payment(@RequestBody UserPaymentDto userPaymentDto){
+        UserPaymentDto savedPayment = paymentService.savePayment(userPaymentDto);
+        return new ResponseEntity<>(savedPayment, HttpStatus.CREATED);
 
     }
 
