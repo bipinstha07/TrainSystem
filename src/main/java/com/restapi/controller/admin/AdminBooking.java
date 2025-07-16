4a51aed7ed5b0299629daeca399b0f8ed09934de
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-07-15 20:02:05 -0500

    Api swagger configuration

--- src/main/java/com/restapi/controller/admin/AdminBooking.java
+++ src/main/java/com/restapi/controller/admin/AdminBooking.java
@@ -2,6 +2,9 @@ package com.restapi.controller.admin;
 
 import com.restapi.dto.BookingDto;
 import com.restapi.service.BookingServiceImp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminBooking {
     private BookingServiceImp bookingServiceImp;
 
+
+    @Operation(
+            summary = "Save Booking",
+            description = "This API saved Booking"
+    )
+    @ApiResponses( value={
+            @ApiResponse(responseCode = "200",description = "Success"),
+            @ApiResponse(responseCode = "500",description = "Internal Server Error")
+    }
+    )
     @PostMapping()
     public ResponseEntity<BookingDto> save(@RequestBody BookingDto bookingDto){
       return new ResponseEntity<>(bookingServiceImp.save(bookingDto), HttpStatus.CREATED);
